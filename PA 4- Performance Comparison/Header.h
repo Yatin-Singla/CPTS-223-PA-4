1ad4e5750b93d58d5bee514ab348a4cb688edb60
Yatin Singla <yatinsingla2@gmail.com>
2018-02-20 07:38:30 -0800

    Added Comment block

--- PA 4- Performance Comparison/Header.h
+++ PA 4- Performance Comparison/Header.h
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <ctime>
 
+using namespace std;
+
 typedef struct stats
 {
 	int inputSequence;
--- PA 4- Performance Comparison/TestDriver.cpp
+++ PA 4- Performance Comparison/TestDriver.cpp
@@ -1,3 +1,15 @@
+/************************************************************************************************
+*	Name: Yatin Singla																			*
+*	Date: 20th February 2018																	*
+*	Programming Assignment 4:																	*
+*	Description: comparing  the  performance  of  the  four  different algorithms 				*
+*	for the maximum subsequence sum  problem.													*
+*	Reference:																					*
+*	A program described in	B.Stroustrup : Wrapping C++ Member Function Calls					*
+*	"The C++ Report" June 2000																	*
+*	http ://www.research.att.com/~bs/wrap_code.cpp												*
+*************************************************************************************************/
+
 #include "Header.h"
 
 int main(void)
