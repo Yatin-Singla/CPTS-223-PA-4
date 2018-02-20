52756822332e011d1555289e9f76f96bc1bc153f
Yatin Singla <yatinsingla2@gmail.com>
2018-02-20 07:49:50 -0800

    Initial Code for MaxSubSum1

---
+++ PA 4- Performance Comparison/Header.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+typedef struct stats
+{
+	int inputSequence;
+	int result;
+	float timeTaken;
+} Stats;
+
+void maxSubSum1(int Arr[], int size, Stats * Seq1Stats);
+void maxSubSum2(int Arr[], int size, Stats * Seq2Stats);
+void maxSubSum3(int Arr[], int size, Stats * Seq3Stats);
+void maxSubSum4(int Arr[], int size, Stats * Seq4Stats);
+
+//functoin which loads integers from a text file into an array and returns the pointer to the array.
+int * LoadArr(FILE * infile, int size);
+
+//functions prints the output to a file.
+void Stat(Stats algorithm, FILE * outfile);
---
+++ PA 4- Performance Comparison/Source.cpp
@@ -0,0 +1,66 @@
+#include "Header.h"
+
+void maxSubSum1(int Arr[], int size, Stats * Seq1Stats)
+{
+	clock_t start = clock();
+	int maxSum = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i; j < size; j++)
+		{
+			int thisSum = 0;
+
+			for (int k = i; k <= j; k++)
+			{
+				thisSum += Arr[k];
+			}
+				
+			if (thisSum > maxSum)
+			{
+				maxSum = thisSum;
+			}
+			
+		}
+	}
+	clock_t end = clock();
+	/*
+	* The number of ticks elapsed during the f() function call
+	* can be calculated by (end - start). The constant
+	* CLOCKS_PER_SEC specifies the relation between a clock tick
+	* and a second (i.e. clock ticks per second). Therefore, dividing
+	* (end - start) by CLOCKS_PER_SEC will give you the elapsed
+	* time in seconds.
+	*/
+	cout << "CPU elapsed time in seconds: "
+		<< (double)(end - start) / CLOCKS_PER_SEC << endl;
+
+	Seq1Stats->result =  maxSum;
+	Seq1Stats->inputSequence = size;
+
+}
+
+void maxSubSum2(int Arr[], int size)
+{
+
+}
+
+void maxSubSum3(int Arr[], int size)
+{
+
+}
+
+void maxSubSum4(int Arr[], int size)
+{
+
+}
+
+int * LoadArr(FILE * infile, int size)
+{
+
+}
+
+void Stat(Stats algorithm, FILE * outfile)
+{
+
+}
